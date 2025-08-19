db16e99dbd4352916ac0123a607188e39ef9490e
Jeremy Powell <jeremy@visionaid.com>
2025-08-19 16:42:09 +1200

    Add basic scaling sample

--- samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
+++ samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
@@ -1,4 +1,5 @@
 ﻿using UIKit;
+using Plugin.Maui.Workarounds.MacCatalyst.Binding;
 
 namespace Plugin.Maui.Workarounds.MacCatalyst.Sample;
 
@@ -7,6 +8,8 @@ public class Program
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
+        MacCatalystWorkarounds.OverrideCatalystScaleFactor();
+
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
         UIApplication.Main(args, null, typeof(AppDelegate));
