9ba3d7dea83bac9892bcd7bb7c37fd12bae311d2
Jeremy Powell <jeremy@visionaid.com>
2025-09-08 21:45:13 +1200

    Redirect NSLog to debug console

--- samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
+++ samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
@@ -7,6 +7,8 @@ public class Program
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
+        Workarounds.RedirectNSLogToDebugConsole();
+
         Workarounds.OverrideCatalystScaleFactor();
 
         // if you want to use a different Application Delegate class from "AppDelegate"
