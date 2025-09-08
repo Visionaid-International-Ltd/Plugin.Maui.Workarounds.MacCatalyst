6267b239df15917f53e639afa2c194857e408356
Jeremy Powell <jeremy@visionaid.com>
2025-09-08 21:45:13 +1200

    Redirect NSLog to debug console

--- src/Plugin.Maui.Workarounds.MacCatalyst/Workarounds.cs
+++ src/Plugin.Maui.Workarounds.MacCatalyst/Workarounds.cs
@@ -1,6 +1,42 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 namespace Plugin.Maui.Workarounds.MacCatalyst;
 
 public static class Workarounds
 {
     public static void OverrideCatalystScaleFactor() => Binding.MacCatalystWorkarounds.OverrideCatalystScaleFactor();
+
+    public static void RedirectNSLogToDebugConsole() => Task.Run(RedirectNSLogToDebugConsoleAsync);
+
+    public static async Task RedirectNSLogToDebugConsoleAsync()
+    {
+        int[] fds = new int[2];
+        if (pipe(fds) == -1)
+        {
+            Debug.WriteLine("Failed to create pipe for NSLog redirection.");
+            return;
+        }
+
+        if (dup2(fds[1], 2) == -1)
+        {
+            Debug.WriteLine("Failed to redirect stderr for NSLog redirection.");
+            return;
+        }
+
+        using Microsoft.Win32.SafeHandles.SafeFileHandle fileHandle = new(fds[0], ownsHandle: true);
+        using FileStream stream = new(fileHandle, FileAccess.Read);
+        using StreamReader sr = new(stream);
+        string? line;
+        while ((line = await sr.ReadLineAsync()) is not null)
+        {
+            Debug.WriteLine($"[NSLog] {line}");
+        }
+    }
+
+    [DllImport("libc")]
+    private static extern int pipe(int[] fds);
+
+    [DllImport("libc")]
+    private static extern int dup2(int oldfd, int newfd);
 }
