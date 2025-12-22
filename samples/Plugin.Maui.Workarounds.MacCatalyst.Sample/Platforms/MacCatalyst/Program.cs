58be5fa03b1eaf73ecb3bea5742f8269f43ed641
Jeremy Powell <jeremy@visionaid.com>
2025-12-23 10:50:51 +1300

    Use Trace for NSLog redirection

--- samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
+++ samples/Plugin.Maui.Workarounds.MacCatalyst.Sample/Platforms/MacCatalyst/Program.cs
@@ -7,7 +7,7 @@ public class Program
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
-        Workarounds.RedirectNSLogToDebugConsole();
+        Workarounds.RedirectNSLogToTrace();
 
         Workarounds.OverrideCatalystScaleFactor();
 
